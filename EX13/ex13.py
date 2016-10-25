e78276bca78df6e79f60ea3e3b2ccf1ce229fde7
flo10696 <florianhermes@online.de>
2016-10-25 16:15:07 +0200

    addes folders & exercizes 1-14

---
+++ EX13/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called", script
+print "The first variable is:", first
+print "The second varaible is:", second
+print "The third varaible is:", third
