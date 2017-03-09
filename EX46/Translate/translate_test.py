63a43e2876b73527a0298fdaf2f1776d9bbdb180
flo10696 <florianhermes@online.de>
2017-03-10 00:53:30 +0100

    updated missing files

---
+++ EX46/Translate/translate_test.py
@@ -0,0 +1,6 @@
+from translation import bing, baidu, iciba, google
+
+print(bing('Hello World', dst = 'de'))
+print(baid('Hello World', dst = 'de'))
+print(iciba('Hello World' dst = 'de'))
+print(google('Hello World', dst = 'de'))
